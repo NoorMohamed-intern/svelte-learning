0b7e4320a271e65b75caa1565e811febc96180f8
NoorMohamed-intern <noormd.intern@gmail.com>
2022-09-10 15:14:14 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import FeedbackList from './components/FeedbackList.svelte'
+	import FeedbackStats from './components/FeedbackStats.svelte'
+	import FeedbackForm from './components/FeedbackForm.svelte'
+
+</script>
+
+<main class="container">
+	<h1 style="text-align: center; text-decoration: underline;"> Simple Svelte App </h1>
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
+</main>
